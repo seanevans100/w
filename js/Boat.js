bea13d0b9035f1fc0f5081c2b82ebd4e056eae5e
seanevans100 <88376332+seanevans100@users.noreply.github.com>
2021-10-30 13:10:10 +0800

    Add files via upload

---
+++ js/Boat.js
@@ -0,0 +1,41 @@
+class Boat {
+  constructor(x, y, width, height, boatPos) {
+  
+    this.body = Bodies.rectangle(x, y, width, height);
+    this.width = width;
+    this.height = height;
+
+    this.image = loadImage("./assets/boat.png");
+    this.boatPosition = boatPos;
+    this.animaton = boatAnimation;
+    this.speed = 0.05;
+    this.isBroken = false;
+    World.add(world, this.body);
+  }
+
+  display() {
+    var angle = this.body.angle;
+    var pos = this.body.position;
+    var index = floor(this.speed % this.animation.length);
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.animation[index], 0, this.boatPosition, this.width, this.height);
+    pop();
+  }
+  animate(){
+    this.speed+=0.05
+  }
+  remove(index){
+    this.animation = brokenBoatAnimation;
+    this.speed = 0.05;
+    this.width = 300
+    this.height = 300
+    this.isBroken = true
+    setTimeout(() => {
+      Matter.World.remove(world,boats[index].body)
+      delete boats[index]
+    }, 2000);
+  }
+}
